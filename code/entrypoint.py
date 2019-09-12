4ca4576c1e13efa417b75208a85cd4c95680094e
Mo Kamioner <mo@lightricks.com>
2019-09-12 11:52:21 +0300

    S3 resize example

--- code/entrypoint.py
+++ code/entrypoint.py
@@ -1,13 +1,44 @@
 # Copyright (c) 2019 Lightricks. All rights reserved.
 
+from io import BytesIO
 import json
+
+from PIL import Image
+import boto3
+
+S3_CLIENT = boto3.client("s3")
+IMAGE_WIDTH_SIZES = [120, 240, 480]
+
+def _extract_bucket_and_key_from_record(record):
+    bucket = record["s3"]["bucket"]["name"]
+    key = record["s3"]["object"]["key"]
+
+    return bucket, key
+
+def _resize_picture(bucket, key, width_size):
+    print("Resizing picture %s/%s to width of %s" % (bucket, key, width_size))
+    file_name = key.split("/")[-1]
+
+    # Load the image from S3
+    original_data = S3_CLIENT.get_object(Bucket=bucket, Key=key)["Body"].read()
+
+    # Resize the image
+    img = Image.open(BytesIO(original_data))
+    width_percent = float(width_size / float(img.size[0]))
+    horizontal_size = int((float(img.size[1]) * width_percent))
+    img = img.resize((width_size, horizontal_size), Image.ANTIALIAS)
+    buffer = BytesIO()
+    img.save(buffer, "JPEG")
+
+    # Save the new resized image to the cloud
+    new_key = "resized/%s_%s" % (width_size, file_name)
+    buffer.seek(0)
+    S3_CLIENT.put_object(Bucket=bucket, Key=new_key, Body=buffer)
 
 def handler(event, context):
     print(json.dumps(event))
 
-    query_parameters = event.get("queryStringParameters") or {}
-    name = query_parameters.get("name", "N/A")
-
-    return {
-        "body": "Hi %s! Welcome to the cloud" % name,
-    }
+    for record in event.get("Records"):
+        bucket, key = _extract_bucket_and_key_from_record(record)
+        for width_size in IMAGE_WIDTH_SIZES:
+            _resize_picture(bucket, key, width_size)
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+set -e
+
 # We want to work with the Lightricks dev account, not production.
 export AWS_PROFILE=dev
 
@@ -7,23 +9,33 @@ export AWS_PROFILE=dev
 # will be easier identify our changes.
 export AWS_DEFAULT_REGION=eu-west-1
 
+# This is where we upload the code for our function. It must exist before we upload and in the same
+# region that the lambda function exists in.
+s3_code_bucket="lightricks-staging-lambda-code-eu-west-1"
+
+# Only create a virtual environment for python if one does not exist
+if [ ! -d venv3 ]; then
+  virtualenv -p /usr/local/bin/python3 venv3
+fi
+
+source venv3/bin/activate
+pip install aws-sam-cli
+
+echo "-- Install the relevant dependencies using the lambda runtime"
+sam build \
+  --template sam-template.yaml \
+  --use-container \
+  --build-dir deploy_package
+
 echo "-- Package code and upload"
 aws cloudformation package \
-  --template-file sam-template.yaml \
-  --s3-bucket lightricks-staging-lambda-code-eu-west-1 \
+  --template-file deploy_package/template.yaml \
+  --s3-bucket $s3_code_bucket \
   --output-template-file .packaged-sam-template.yaml
 
 echo "-- Deploy lambda function"
 aws cloudformation deploy \
   --template-file .packaged-sam-template.yaml \
   --stack-name my-first-lambda \
-  --capabilities CAPABILITY_IAM
-
-url=$(
-  aws cloudformation describe-stacks \
-    --stack-name my-first-lambda \
-    --output text \
-    --query "Stacks[0].Outputs[?OutputKey=='Url'].OutputValue"
-)
-
-echo "-- Use the following URL to access your new API: ${url}"
+  --capabilities CAPABILITY_IAM \
+  --no-fail-on-empty-changeset
