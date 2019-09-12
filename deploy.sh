d7e39de7d9db32fa78f5372325433e935ee0a409
Mo Kamioner <mo@lightricks.com>
2019-09-12 10:58:27 +0300

    Initial commit.

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+# We want to work with the Lightricks dev account, not production.
+export AWS_PROFILE=dev
+
+# Let's deploy to Ireland for this example. There are not a lot of existing resources there so it
+# will be easier identify our changes.
+export AWS_DEFAULT_REGION=eu-west-1
+
+echo "-- Package code and upload"
+aws cloudformation package \
+  --template-file sam-template.yaml \
+  --s3-bucket lightricks-staging-lambda-code-eu-west-1 \
+  --output-template-file .packaged-sam-template.yaml
+
+echo "-- Deploy lambda function"
+aws cloudformation deploy \
+  --template-file .packaged-sam-template.yaml \
+  --stack-name my-first-lambda \
+  --capabilities CAPABILITY_IAM
+
+url=$(
+  aws cloudformation describe-stacks \
+    --stack-name my-first-lambda \
+    --output text \
+    --query "Stacks[0].Outputs[?OutputKey=='Url'].OutputValue"
+)
+
+echo "-- Use the following URL to access your new API: ${url}"
